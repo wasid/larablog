66efa254976c44d85caf01a1a5ccf5ea703b0d21
Wasid Hossain <wasidmail@gmail.com>
2016-09-26 13:47:02 +0000

    Done till create users without photo

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Http\Requests\UsersRequest;
+
+use App\User;
+
+use App\Role;
+
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        
+        return view('admin.users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::lists('name', 'id')->all();
+        return view('admin.users.create', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UsersRequest $request)
+    {
+        User::create($request->all());
+        
+        return redirect()->route('admin.users.index');
+        
+        
+        //return $request->all();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.admin')
+
+@section('content')
+
+@include('includes.form_error')
+
+<h1>Create Users</h1>
+    
+    {!! Form::open([ 'method' => 'post', 'action' => 'AdminUsersController@store', 'files' => true]) !!}
+    
+    <div class="form-group">
+        
+        {!! Form::label('name', 'Name:'); !!}
+        
+        {!! Form::text('name', null, ['class' => 'form-control']); !!}
+        
+    </div>
+    
+    <div class="form-group">
+        
+        {!! Form::label('email', 'Email:'); !!}
+        
+        {!! Form::email('email', null, ['class' => 'form-control']); !!}
+        
+    </div>
+    
+    <div class="form-group">
+        
+        {!! Form::label('role_id', 'Role:'); !!}
+        
+        {!! Form::select('role_id', ['' => 'Choose One'] + $roles, null, ['class' => 'form-control']); !!}
+        
+    </div>
+    
+    <div class="form-group">
+        
+        {!! Form::label('is_active', 'Status:'); !!}
+        
+        {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), 0, ['class' => 'form-control']); !!}
+        
+    </div>
+    
+    <div class="form-group">
+        
+        {!! Form::label('file', 'File:'); !!}
+        
+        {!! Form::file('file', null, ['class' => 'form-control']); !!}
+        
+    </div>
+
+    <div class="form-group">
+        
+        {!! Form::label('password', 'Password:'); !!}
+        
+        {!! Form::password('password', ['class' => 'form-control']); !!}
+        
+    </div>
+    
+    <div class="form-goup">
+        
+        {!! Form::submit('Create User', ['class' => 'btn btn-primary']); !!}
+        
+    </div>
+    
+    {!! Form::close() !!}
+    
+@stop
